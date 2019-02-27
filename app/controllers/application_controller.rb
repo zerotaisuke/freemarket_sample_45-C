60e4022103dabf1002fce8532500a937ad46f8c8
hikaru <hikaru.fujita.0903@gmail>
2019-02-27 16:26:53 +0900

    Add_nested_for_users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_paramaters, if: :devise_controller?
+
+private
+def configure_permitted_paramaters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:profile_attributes => [:user_id, :name]])
+
+  # update処理で使うかもしれないので一旦保存
+  # devise_parameter_sanitizer.permit(:account_update, keys: [:profile_attributes => [:user_id, :name, :body]])
+end
+
 end
