ee3b7189865802c48144330d23d87fac017b484c
hikaru <hikaru.fujita.0903@gmail>
2019-02-27 15:19:40 +0900

    Add_views_of_devise

--- db/migrate/20190226103616_create_profiles.rb
+++ db/migrate/20190226103616_create_profiles.rb
@@ -7,11 +7,11 @@ class CreateProfiles < ActiveRecord::Migration[5.2]
       t.date    :birthdate,       null: false
       t.integer :postal_code,     null: false
       t.string  :prefecture,      null: false
-      t.string  :municipality,
-      t.string  :block,
+      t.string  :municipality
+      t.string  :block
       t.integer :sales_money,     null: false, default: 0
       t.integer :mercari_point,   null: false, default: 0
-      t.string  :payment_methods  null: false
+      t.string  :payment_methods, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_072345) do
+ActiveRecord::Schema.define(version: 2019_02_26_103616) do
+
+  create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "name", null: false
+    t.string "nickname", null: false
+    t.date "birthdate", null: false
+    t.integer "postal_code", null: false
+    t.string "prefecture", null: false
+    t.string "municipality"
+    t.string "block"
+    t.integer "sales_money", default: 0, null: false
+    t.integer "mercari_point", default: 0, null: false
+    t.string "payment_methods", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
