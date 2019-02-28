2e28e60741a328bf1185fcfa9ba80bb6f1c4166d
hikaru <hikaru.fujita.0903@gmail>
2019-02-28 18:51:05 +0900

    Add_validation_for_sign_up

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,9 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :nickname, presence: true, length: { maximum: 20 }
+  validates :birthdate, presence: true
+  validates :postal_code, presence: true
+  validates :municipality, presence: true
 end
