5d162d9e0ab0dbbe777bee8c2f3a429ff5d6db03
zerotaisuke <taisuke@taisukenoMacBook-Pro.local>
2019-03-04 18:09:27 +0900

    Gitが抜けてた

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "freemarket_sample_45-C"
-set :repo_url, "git@github.com:zerotaisuke/freemarket_sample_45-C"
+set :repo_url, "git@github.com:zerotaisuke/freemarket_sample_45-C.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
