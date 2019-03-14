6e4d2149b440e0f525d1ba38a6e46cbb19b9e807
Ryo <Ryo@uetotakashinoMacBook-puro.local>
2019-03-14 10:56:21 +0900

    Edit-csv(カテゴリーを記述したcsvファイルの更新)
    # WHAT
    カテゴリーを記述したcsvファイルの更新
    カテゴリーモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -53,12 +53,6 @@ ActiveRecord::Schema.define(version: 2019_03_11_051900) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "userinfos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
