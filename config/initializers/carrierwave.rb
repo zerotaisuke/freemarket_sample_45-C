07f0fded067f36d67123107ebfd838f5ffbdb6b3
zerotaisuke <taisuke@taisukenoMacBook-Pro.local>
2019-03-21 19:13:53 +0900

    config.asset_hostを編集

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'freemarket-sample45c'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-sample45c'
+  config.asset_host = 'https://freemarket-sample45c.s3-ap-northeast-1.amazonaws.com'
 end
